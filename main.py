1aa421e95591c656965466a386e01db8f389b3e8
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2023-02-14 10:22:48 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ print("WELCOME TO THE CAUGHT SPEEDING PROGRAM \nEnter the speeds and speed limit
 
 # Asking the user to enter the speed and speed limit for three drivers
 print("DRIVER 1")
-driver1_speed = input("Enter speed for driver 1:")
-driver1_speed_limit = input("Enter speed limit for driver 1:")
+driver1_speed = int(input("Enter speed for driver 1:"))
+driver1_speed_limit = int(input("Enter speed limit for driver 1:"))
 
 print("DRIVER 2")
-driver2_speed = input("Enter speed for driver 2:")
-driver2_speed_limit = input("Enter speed limit for driver 2:")
+driver2_speed = int(input("Enter speed for driver 2:"))
+driver2_speed_limit = int(input("Enter speed limit for driver 2:"))
 
 print("DRIVER 3")
-driver3_speed = input("Enter speed for driver 3:")
-driver3_speed_limit = input("Enter speed limit for driver 3:")
+driver3_speed = int(input("Enter speed for driver 3:"))
+driver3_speed_limit = int(input("Enter speed limit for driver 3:"))
 
 # Function to Determine Ticket Type
 def determineTicket(speed, limit):
-    if speed > limit + 40:
-        print("Really Big Ticket")
-    elif speed > limit + 20 and speed <= limit + 40:
-        print("Big Ticket")
-    elif speed > limit and speed <= limit + 20:
-        print("Small Ticket")
+    if speed - 40 > limit:
+        return "Really Big Ticket"
+    elif speed - 20 > limit and speed - 40 <= limit:
+        return "Big Ticket"
+    elif speed > limit and speed - 20 <= limit:
+        return "Small Ticket"
     elif speed <= limit:
-        print("No Ticket")
+        return "No Ticket"
 
 # Determining the type of ticket based on user input
 driver1_ticket = determineTicket(driver1_speed, driver1_speed_limit)    
 driver2_ticket = determineTicket(driver1_speed, driver1_speed_limit)    
 driver3_ticket = determineTicket(driver1_speed, driver1_speed_limit)    
 
-print(f"Driver 1 Ticket Type: {driver1_ticket}")
-print(f"Driver 2 Ticket Type: {driver2_ticket}")
-print(f"Driver 3 Ticket Type: {driver3_ticket}")
+print(f"\nDriver 1 Ticket Type: {driver1_ticket}")
+print(f"\nDriver 2 Ticket Type: {driver2_ticket}")
+print(f"\nDriver 3 Ticket Type: {driver3_ticket}")
